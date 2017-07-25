02189da71ce3b68336061ed93907b0a3ee43842b
Alex Brinkman <abrink@commercekitchen.com>
2017-07-25 16:55:38 -0600

    [CHG] Impl of a static homepage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
@@ -67,5 +67,5 @@ Rails.application.configure do
     Bullet.rails_logger = true
     Bullet.add_footer = true
   end
-  
+
 end
