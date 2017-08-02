f920c22df863781ff8e6e02aae0bd5c9039bf79e
Alex Brinkman <abrink@commercekitchen.com>
2017-08-02 14:01:29 -0600

    [REFAC] More code cleanup from import

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -10,7 +10,7 @@ module LessonsHelper
       story_line_url = "/storylines/#{lesson_id}/#{directory}/story.html"
       content_tag(:iframe, nil, src: "#{story_line_url}", class: "story_line")
     else
-      content_tag(:p, "No lesson available at this point.", class: "note")
+      content_tag(:p, "There is no available lesson.", class: "note")
     end
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,6 +4,8 @@ class Lesson < ApplicationRecord
   friendly_id :title, use: :slugged
 
   belongs_to :course
+  has_attached_file :story_line, url: "/system/lessons/story_lines/:id/:style/:basename.:extension"
+  before_post_process :skip_for_zip
 
   # TODO: We need to make lesson titles unique per course, but not site-wide.
   validates :title, length: { maximum: 90 }, presence: true # , uniqueness: true
@@ -13,14 +15,8 @@ class Lesson < ApplicationRecord
   validates :seo_page_title, length: { maximum: 90 }
   validates :meta_desc, length: { maximum: 156 }
   validates :pub_status, presence: true, inclusion: { in: %w(P D A), message: "%{value} is not a valid status" }
-
-  # validates :story_line, attachment_presence: true
-  # validates_with AttachmentPresenceValidator, attributes: :story_line
-
-  has_attached_file :story_line, url: "/system/lessons/story_lines/:id/:style/:basename.:extension"
-  before_post_process :skip_for_zip
-  validates_attachment_content_type :story_line, content_type: ["application/zip", "application/x-zip"],
-    message: ", Please provide a .zip Articulate StoryLine File."
+  validates_attachment :story_line, presence: true,
+    content_type: { content_type: ["application/zip", "application/x-zip"] }, size: { in: 0..100.megabytes }
 
   before_destroy :delete_associated_asl_files
   before_destroy :delete_associated_user_completions
