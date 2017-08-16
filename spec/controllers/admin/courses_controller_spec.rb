701a58cbe8321fd22d29bfcbf5e1a4655ccaeadb
Alex Brinkman <abrink@commercekitchen.com>
2017-08-16 09:24:53 -0600

    [REFAC] Adding test coverage

--- spec/controllers/admin/courses_controller_spec.rb
+++ spec/controllers/admin/courses_controller_spec.rb
@@ -13,123 +13,132 @@ describe Admin::CoursesController do
 
   end
 
-  # describe "GET #index" do
-  #
-  #   it "assigns all pages" do
-  #     @admin = FactoryGirl.create(:admin)
-  #     sign_in @admin
-  #     @page1 = FactoryGirl.create(:page)
-  #     @page2 = FactoryGirl.create(:page)
-  #     get :index
-  #     expect(assigns(:pages)).to contain_exactly(@page1, @page2)
-  #   end
-  #
-  #   pending "permissions"
-  #
-  # end
-  #
-  # describe "POST #create" do
-  #
-  #   let(:valid_attributes) do
-  #     {
-  #       title: "New Page",
-  #       body: "<p>Body</p>",
-  #       pub_status: "published",
-  #       author: "admin",
-  #       seo_title: "SEO Title",
-  #       meta_desc: "Meta Desc"
-  #     }
-  #   end
-  #
-  #   let(:invalid_attributes) do
-  #     {
-  #       title: "",
-  #       body: "",
-  #       author: ""
-  #     }
-  #   end
-  #
-  #   it "correctly assigns the passed in info" do
-  #     @admin = FactoryGirl.create(:admin)
-  #     sign_in @admin
-  #     post :create, params: { page: valid_attributes }
-  #     page = Page.last
-  #     expect(page.title).to eq "New Page"
-  #     expect(page.slug).to eq "new-page"
-  #     expect(page.body).to eq "<p>Body</p>"
-  #     expect(page.pub_status).to eq "published"
-  #     expect(page.author).to eq "admin"
-  #     expect(page.seo_title).to eq "SEO Title"
-  #     expect(page.meta_desc).to eq "Meta Desc"
-  #     # expect(page.published_at) # TODO: impl
-  #   end
-  #
-  #   it "renders the new view if there is missing information" do
-  #     @admin = FactoryGirl.create(:admin)
-  #     sign_in @admin
-  #     post :create, params: { page: invalid_attributes }
-  #     expect(response).to render_template :new
-  #   end
-  #
-  # end
-  #
-  # describe "GET #edit" do
-  #
-  #   it "assigns the given instance of a page" do
-  #     @admin = FactoryGirl.create(:admin)
-  #     sign_in @admin
-  #     @page = FactoryGirl.create(:page)
-  #     get :edit, params: { id: @page.id }
-  #     expect(assigns(:page)).to eq @page
-  #   end
-  #
-  # end
-  #
-  # describe "PUT #update" do
-  #
-  #   let(:valid_attributes) do
-  #     {
-  #       title: "Updated Page",
-  #       body: "<p>Updated Body</p>",
-  #       pub_status: "archived",
-  #       author: "admin-2",
-  #       seo_title: "Updated SEO Title",
-  #       meta_desc: "Updated Meta Desc"
-  #     }
-  #   end
-  #
-  #   let(:invalid_attributes) do
-  #     {
-  #       title: "",
-  #       body: "",
-  #       author: ""
-  #     }
-  #   end
-  #
-  #   it "correctly assigns the passed in info" do
-  #     @page = FactoryGirl.create(:page)
-  #     @admin = FactoryGirl.create(:admin)
-  #     sign_in @admin
-  #     put :update, params: { id: @page.id, page: valid_attributes }
-  #     @page.reload
-  #     expect(@page.title).to eq "Updated Page"
-  #     # expect(@page.slug).to eq "new-page"
-  #     expect(@page.body).to eq "<p>Updated Body</p>"
-  #     expect(@page.pub_status).to eq "archived"
-  #     expect(@page.author).to eq "admin-2"
-  #     expect(@page.seo_title).to eq "Updated SEO Title"
-  #     expect(@page.meta_desc).to eq "Updated Meta Desc"
-  #     # expect(@page.published_at) # TODO: impl
-  #   end
-  #
-  #   it "renders the edit view if there is missing information" do
-  #     @page = FactoryGirl.create(:page)
-  #     @admin = FactoryGirl.create(:admin)
-  #     sign_in @admin
-  #     put :update, params: { id: @page.id, page: invalid_attributes }
-  #     expect(response).to render_template :edit
-  #   end
-  #
-  # end
+  describe "GET #index" do
+
+    it "assigns all courses that aren't archived" do
+      @admin = FactoryGirl.create(:admin)
+      sign_in @admin
+      @course1 = FactoryGirl.create(:course)
+      @course2 = FactoryGirl.create(:course)
+      @course3 = FactoryGirl.create(:course, pub_status: "A")
+      get :index
+      expect(assigns(:courses)).to contain_exactly(@course1, @course2)
+    end
+
+    pending "permissions"
+
+  end
+
+  describe "POST #create" do
+
+    let(:valid_attributes) do
+      {
+        title: "Title",
+        summary: "Summary",
+        description: "Description",
+        notes: "Notes",
+        contributor: "Contributor",
+        pub_status: "P",
+        seo_page_title: "SEO Course Title",
+        meta_desc: "Meta Desc"
+      }
+    end
+
+    let(:invalid_attributes) do
+      {
+        title: "",
+        summary: "",
+        description: "",
+        notes: "",
+        contributor: "",
+        pub_status: ""
+      }
+    end
+
+    it "correctly assigns the passed in info" do
+      @admin = FactoryGirl.create(:admin)
+      sign_in @admin
+      post :create, params: { course: valid_attributes }
+      course = Course.last
+      expect(course.title).to eq "Title"
+      expect(course.summary).to eq "Summary"
+      expect(course.description).to eq "Description"
+      expect(course.notes).to eq "Notes"
+      expect(course.contributor).to eq "Contributor"
+      expect(course.seo_page_title).to eq "SEO Course Title"
+      expect(course.meta_desc).to eq "Meta Desc"
+    end
+
+    it "renders the new view if there is missing information" do
+      @admin = FactoryGirl.create(:admin)
+      sign_in @admin
+      post :create, params: { course: invalid_attributes }
+      expect(response).to render_template :new
+    end
+
+  end
+
+  describe "GET #edit" do
+
+    it "assigns the given instance of a course" do
+      @admin = FactoryGirl.create(:admin)
+      sign_in @admin
+      @course = FactoryGirl.create(:course)
+      get :edit, params: { id: @course.id }
+      expect(assigns(:course)).to eq @course
+    end
+
+  end
+
+  describe "PUT #update" do
+
+    let(:valid_attributes) do
+      {
+        title: "Updated Title",
+        summary: "Updated Summary",
+        description: "Updated Description",
+        notes: "Updated Notes",
+        contributor: "Updated Contributor",
+        pub_status: "A",
+        seo_page_title: "Updated SEO Course Title",
+        meta_desc: "Updated Meta Desc"
+      }
+    end
+
+    let(:invalid_attributes) do
+      {
+        title: "",
+        summary: "",
+        description: "",
+        notes: "",
+        contributor: "",
+        pub_status: ""
+      }
+    end
+
+    it "correctly assigns the passed in info" do
+      @course = FactoryGirl.create(:course)
+      @admin = FactoryGirl.create(:admin)
+      sign_in @admin
+      put :update, params: { id: @course.id, course: valid_attributes }
+      @course.reload
+      expect(@course.title).to eq "Updated Title"
+      expect(@course.summary).to eq "Updated Summary"
+      expect(@course.description).to eq "Updated Description"
+      expect(@course.notes).to eq "Updated Notes"
+      expect(@course.contributor).to eq "Updated Contributor"
+      expect(@course.seo_page_title).to eq "Updated SEO Course Title"
+      expect(@course.meta_desc).to eq "Updated Meta Desc"
+    end
+
+    it "renders the edit view if there is missing information" do
+      @course = FactoryGirl.create(:course)
+      @admin = FactoryGirl.create(:admin)
+      sign_in @admin
+      put :update, params: { id: @course.id, course: invalid_attributes }
+      expect(response).to render_template :edit
+    end
+
+  end
 
 end
