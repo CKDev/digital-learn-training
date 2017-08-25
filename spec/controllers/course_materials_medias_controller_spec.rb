dd75938e435047d3350613ab9ab1273b0b455e8a
Alex Brinkman <abrink@commercekitchen.com>
2017-08-25 09:38:36 -0600

    [REFAC] Add test coverage around file downloads

---
+++ spec/controllers/course_materials_medias_controller_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+describe CourseMaterialsMediasController do
+
+  describe "GET #show" do
+
+    it "assigns the requested instance course materials media" do
+      @course_material_media = FactoryGirl.create(:course_material_media)
+      @course_material = @course_material_media.course_material
+      get :show, params: { course_material_id: @course_material.id, id: @course_material_media.id }
+      expect(response).to have_http_status(:success) # TODO: what can we check here?
+    end
+
+  end
+
+  describe "GET #index" do
+
+    it "assigns all the requested instances of a course materials medias" do
+      @course_material_media1 = FactoryGirl.create(:course_material_media)
+      @course_material = @course_material_media1.course_material
+      @course_material_media2 = FactoryGirl.create(:course_material_media, course_material: @course_material, media_file_name: "test2.png")
+      get :index, params: { course_material_id: @course_material.id }
+      expect(response).to have_http_status(:success) # TODO: what can we check here?
+    end
+
+  end
+
+end
