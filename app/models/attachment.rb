444697afe27bcae69eebe59c8b28f991b622d26d
Alex Brinkman <abrink@commercekitchen.com>
2017-09-10 13:15:41 -0600

    [BUG] File uploads need to allow for .story
    I have the correct server side check, but the browser narrowing on
    file upload didn't include .story files.  Since it seems every browser
    is interpreting them as a different type, I'm just allowing all file
    types in the browser and relying on the server for validation.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,7 +4,6 @@ class Attachment < ApplicationRecord
 
   validates_attachment_content_type :document,
     content_type: Constants.attachment_content_types, message: "only PDF, Word, PowerPoint, Excel, or Story files are allowed."
-
   validates :doc_type, allow_blank: true, inclusion: { in: %w(supplemental post-course), message: "%{value} is not a doc_type" }
 
   scope :supplemental_attachments, -> { where(doc_type: "supplemental") }
