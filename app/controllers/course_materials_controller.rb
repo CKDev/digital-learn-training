d3440588a966de198e203a832c2d8a319feec822
Alex Brinkman <abrink@commercekitchen.com>
2017-09-13 12:11:06 -0600

    [BUG] / vs /courses course view was different
    The view on the homepage should exactly match the /courses view. I
    had these as two separate files which eventually got out of sync.  I
    DRY'd this up to fix the issue.

--- app/controllers/course_materials_controller.rb
+++ app/controllers/course_materials_controller.rb
@@ -2,6 +2,7 @@ class CourseMaterialsController < ApplicationController
 
   def index
     @categories = Category.includes(sub_categories: :course_materials).all
+    @getting_started = @categories.where(tag: "Getting Started")
     @hardware = @categories.where(tag: "Hardware")
     @software_and_applications = @categories.where(tag: "Software & Applications")
     @job_and_career = @categories.where(tag: "Job & Career")
