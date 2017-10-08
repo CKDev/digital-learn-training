92719aa5ca0475853ba47196a65a19b592ac11fd
Alex Brinkman <abrink@commercekitchen.com>
2017-10-08 09:26:00 -0600

    [CHG] Remove the Peer Discussions discourse links
    The Discourse site will no longer be used, so this removes all of that
    functionality.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @recent_topics = discourse_wrapper.recent_topics
     @courses = Course.published.limit(2)
     @categories = Category.includes(sub_categories: :course_materials).all
     @getting_started = @categories.where(tag: "Getting Started")
@@ -9,12 +8,6 @@ class HomeController < ApplicationController
     @software_and_applications = @categories.where(tag: "Software & Applications")
     @job_and_career = @categories.where(tag: "Job & Career")
     @blank_template = CourseMaterial.find_by(title: "Course Templates")
-  end
-
-  private
-
-  def discourse_wrapper
-    @discourse_wrapper ||= Discourse::DiscourseApiWrapper.new
   end
 
 end
