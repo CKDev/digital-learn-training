233a45afad6a6c0857aa69ca3817858236276784
Tom Reis <tom@ckdtech.co>
2021-07-14 10:41:12 -0600

    Require title for organizations

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Organization do
+  describe 'validations' do
+    let(:subject) { Organization.new }
+
+    it 'requires a title'do
+      expect(subject).not_to be_valid
+      expect(subject.errors.full_messages).to include("Title can't be blank")
+    end
+  end
 end
