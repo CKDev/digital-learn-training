0dc3c2f1ed08ba956f44254dbcc0c8574d3927b1
Tom Reis <tom@ckdtech.co>
2021-08-29 12:39:54 -0600

    Allow egress from load balancer

--- ops/modules/load_balancer/security_group.tf
+++ ops/modules/load_balancer/security_group.tf
@@ -10,4 +10,12 @@ resource "aws_security_group" "load_balancer_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic from load balancer"
+  }
 }
