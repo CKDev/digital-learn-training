d66515b08281d490766e9aefd5e7f8525f6ce4b7
Tom Reis <tom@ckdtech.co>
2021-09-01 21:16:36 -0600

    Remove capistrano from project

--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-set :bundle_without, %w{staging test}.join(" ")
-
-set :branch, "main"
-
-server "training.digitallearn.org",
-  user: fetch(:application),
-  port: 1022,
-  roles: %w{web app db},
-  primary: true
