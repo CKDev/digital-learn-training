bc5fb671822fbff61fd1353504854ea2cc231407
Tom Reis <tom@ckdtech.co>
2023-03-21 22:54:53 -0600

    Skip extra tempfile writes/reads

--- app/services/attachment_zipper.rb
+++ app/services/attachment_zipper.rb
@@ -1,29 +1,23 @@
 class AttachmentZipper
-  def initialize(course_material, files)
+  def initialize(course_material, attachments)
     @course_title = course_material.title.parameterize(separator: '_')
-    @files = files
+    @attachments = attachments
   end
 
-  def create_zip(attachment_name)
+  def create_zip(attachment_type)
     archive_tempfile = Tempfile.new("#{@course_title}_archive", "tmp")
 
-    # Avoid premature garbage collection by keeping tempfiles in array
-    s3_tempfiles = []
     ::Zip::File.open(archive_tempfile.path, ::Zip::File::CREATE) do |zipfile|
-      @files.each do |file|
-        if Rails.application.config.s3_enabled
-          s3_tempfile = Tempfile.new("s3_contents", "tmp")
-          s3_tempfiles << s3_tempfile
-          s3_tempfile << AttachmentReader.new(file).read_attachment_data(attachment_name)
-          s3_tempfile.close
-          zipfile.add(file.send("#{attachment_name}_file_name"), s3_tempfile.path)
-        else
-          zipfile.add(file.send("#{attachment_name}_file_name"), file.send(attachment_name).path)
+      @attachments.each do |attachment|
+        filename = attachment.send("#{attachment_type}_file_name")
+        file_data = AttachmentReader.new(attachment).read_attachment_data(attachment_type)
+
+        zipfile.get_output_stream(filename) do |file|
+          file.puts file_data
         end
       end
     end
 
-    s3_tempfiles = nil
     open(archive_tempfile)
   end
 end
