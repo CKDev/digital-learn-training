a04981887add83ea6b400f8a80d605f14aa46c09
Tom Reis <tom@showami.com>
2024-07-31 21:57:58 -0600

    Check for empty emails correctly

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ApplicationMailer
   def new_access_request(access_request_id)
     @access_request = AccessRequest.find(access_request_id)
     emails = @access_request.organization.access_request_emails
-    return unless email.present?
+    return if (emails || []).empty?
     subject = "New DigitalLearn Collaborator Access Request"
     mail(to: emails, subject: subject)
   end
