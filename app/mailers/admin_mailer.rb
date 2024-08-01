fc886e9905d76f5f6bceadee4fc4f406c1838ceb
Tom Reis <tom@showami.com>
2024-07-31 21:27:31 -0600

    Don't use subdomain for admin access request links

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,6 @@ class AdminMailer < ApplicationMailer
   def new_access_request(access_request_id)
     @access_request = AccessRequest.find(access_request_id)
     email = @access_request.organization.access_request_email
-    @mailer_subdomain = SubdomainBuilder.new(@access_request.organization).build_subdomain
     return unless email.present?
     subject = "New DigitalLearn Collaborator Access Request"
     mail(to: email, subject: subject)
