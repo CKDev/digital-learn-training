f50d70589fdfeb77f22c7866c34e5afc72ffbd2b
Tom Reis <tom@showami.com>
2024-07-31 22:26:11 -0600

    Fix logo in admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,6 +3,12 @@ class AdminMailer < ApplicationMailer
     @access_request = AccessRequest.find(access_request_id)
     emails = @access_request.organization.access_request_emails
     return if (emails || []).empty?
+
+    # Mailer layout variables
+    # Note: The @mailer_subdomain isn't used for the access request link,
+    # but it's used for the logo link from the mailer layout
+    @organization_subdomain = @access_request.organization.subdomain # Logo prefix
+    @mailer_subdomain = SubdomainBuilder.new(att).build_subdomain # Link subdomain
     subject = "New DigitalLearn Collaborator Access Request"
     mail(to: emails, subject: subject)
   end
