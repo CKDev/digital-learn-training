226ee2fb9cb600051c9e49f0e58778ba1b987922
Tom Reis <tom@showami.com>
2024-07-31 20:58:50 -0600

    Use correct subdomains in email links

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ApplicationMailer
   def new_access_request(access_request_id)
     @access_request = AccessRequest.find(access_request_id)
     email = @access_request.organization.access_request_email
-    @mailer_subdomain = @access_request.organization.subdomain
+    @mailer_subdomain = SubdomainBuilder.new(@access_request.organization).build_subdomain
     return unless email.present?
     subject = "New DigitalLearn Collaborator Access Request"
     mail(to: email, subject: subject)
