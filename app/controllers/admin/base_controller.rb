a7ed913999c355333b2203410949d4751f213ca1
Tom Reis <tom@showami.com>
2024-12-24 17:00:36 -0700

    Remove v2 UI for admins

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,6 +3,7 @@ module Admin
     before_action :authenticate_user!
     before_action :authorize_admin!
     before_action :enable_admin_sidebar
+    before_action :unset_ui_v2
 
     def authorize_admin!
       unless current_user.admin?
@@ -15,6 +16,10 @@ module Admin
 
     def enable_admin_sidebar
       @include_admin_sidebar = true
+    end
+
+    def unset_ui_v2
+      @use_ui_v2 = false
     end
   end
 end
