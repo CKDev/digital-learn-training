008e140f73fd7fffe4284300ce8a47835b41d527
Tom Reis <tom@showami.com>
2026-08-19 11:50:12 -0600

    Allow subsites to only allow trainers and admins

--- app/controllers/learners_sessions_controller.rb
+++ app/controllers/learners_sessions_controller.rb
@@ -17,14 +17,21 @@ class LearnersSessionsController < ApplicationController
     )
 
     user_info = token.get('/api/v1/me').parsed # TODO: Update to learners site /me endpoint?
-    user = User.find_or_initialize_by(email: user_info['email'])
-    user.provider = 'dl_sso'
-    user.save!
     subdomain = user_info['organization_subdomain']
     org_admin = user_info['is_org_admin']
+    trainer = user_info['is_trainer'] # Requires the learners site /me endpoint to expose this field
 
     organization = Organization.find_by(subdomain: subdomain)
     raise OrganizationNotFoundError, "Organization not found for subdomain: #{subdomain}" if organization.blank?
+
+    if organization.trainers_only && !org_admin && !trainer
+      redirect_to root_path, alert: 'Your account does not have access to training materials for this organization.'
+      return
+    end
+
+    user = User.find_or_initialize_by(email: user_info['email'])
+    user.provider = 'dl_sso'
+    user.save!
 
     if org_admin
       user.update!(admin: true) if subdomain == 'www'
